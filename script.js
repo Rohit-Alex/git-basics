a404fffc7d47c40420a55fc8b258a7624ecef7d1
Rohit-Alex <rohit5012000@gmail.com>
2024-09-15 11:31:53 +0530

    feat: initialized repo

---
+++ script.js
@@ -0,0 +1,22 @@
+const deepClone = (obj) => {
+  if (obj === null || typeof obj !== "object") {
+    return obj;
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.map((item) => deepClone(item));
+  }
+
+  const clonedObj = {};
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      clonedObj[key] = deepClone(obj[key]);
+    }
+  }
+  return clonedObj;
+};
+
+// Example usage:
+const originalObj = { a: 1, b: { c: 2, d: [3, 4] } };
+const clonedObj = deepClone(originalObj);
+console.log(clonedObj);
